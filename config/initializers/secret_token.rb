1a8db2cfa5e1782165e045e2be11328c44202868
Josh Moore <salsasombrero@hotmail.com>
2013-04-15 21:20:12 -0400

    Server Ping v0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServerPing::Application.config.secret_token = 'dbd71519c7875a94e386bf12dc2f9c8afeb8150e3f029c2b031da45b20efcd6e479c3d5ffdce92f23eb256c71c4b095d0fb85593abbfbfd47b29a98a53a9937a'
